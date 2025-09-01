ee5a571ed66db687bea99530d85989917da6f6c6
Thiago Contelli <thiagocontellid@gmail.com>
2025-09-01 20:37:54 -0300

    Add project files.

---
+++ BrownianMotion/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BrownianMotion")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BrownianMotion.Pages")]
