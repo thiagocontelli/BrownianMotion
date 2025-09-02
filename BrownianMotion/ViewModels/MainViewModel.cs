12cbea762f70d489969e52a10855d861ba851f42
Thiago Contelli <thiagocontellid@gmail.com>
2025-09-01 22:26:53 -0300

    Create form binding the view model

--- BrownianMotion/ViewModels/MainViewModel.cs
+++ BrownianMotion/ViewModels/MainViewModel.cs
@@ -1,7 +1,41 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace BrownianMotion.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private double sigma = 20;
+    [ObservableProperty]
+    private double mean = 2;
+    [ObservableProperty]
+    private double initialPrice = 100.0;
+    [ObservableProperty]
+    private int numDays = 252;
+    [ObservableProperty]
+    private int numSimulations = 1;
+
+    [ObservableProperty]
+    private ObservableCollection<double[]> simulations = new();
+
+
+    [RelayCommand]
+    private void GenerateSimulation()
+    {
+        Simulations.Clear();
+
+        for (int i = 0; i < NumSimulations; i++)
+        {
+            var brownianMotion = new Models.BrownianMotion(
+                Sigma / 100,
+                Mean / 100,
+                InitialPrice,
+                NumDays
+            );
+
+            Simulations.Add(brownianMotion.Generate());
+        }
+    }
 }
