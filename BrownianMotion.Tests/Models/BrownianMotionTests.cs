715d0e4f61356b35d0dc4dc9f95ffdf02b080b6e
Thiago Contelli <thiagocontellid@gmail.com>
2025-09-02 20:40:33 -0300

    Add unit tests

---
+++ BrownianMotion.Tests/Models/BrownianMotionTests.cs
@@ -0,0 +1,47 @@
+﻿namespace BrownianMotion.Tests
+{
+    public class BrownianMotionTests
+    {
+        [Fact]
+        public void Generate_ShouldReturnArrayWithCorrectLength()
+        {
+            int numDays = 10;
+            var bm = new Models.BrownianMotion(0.1, 0.05, 100, numDays);
+
+            var prices = bm.Generate();
+
+            Assert.Equal(numDays, prices.Length);
+        }
+
+        [Fact]
+        public void Generate_ShouldStartWithInitialPrice()
+        {
+            double initialPrice = 100;
+            var bm = new Models.BrownianMotion(0.1, 0.05, initialPrice, 10);
+
+            var prices = bm.Generate();
+
+            Assert.Equal(initialPrice, prices[0], precision: 5);
+        }
+
+        [Fact]
+        public void Generate_ShouldReturnPositivePrices()
+        {
+            var bm = new Models.BrownianMotion(0.2, 0.01, 100, 50);
+
+            var prices = bm.Generate();
+
+            Assert.All(prices, price => Assert.True(price > 0));
+        }
+
+        [Fact]
+        public void Generate_ShouldVaryOverTime()
+        {
+            var bm = new Models.BrownianMotion(0.3, 0.01, 100, 20);
+
+            var prices = bm.Generate();
+
+            Assert.Contains(prices, price => Math.Abs(price - 100) > 0.0001);
+        }
+    }
+}
---
+++ BrownianMotion.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,48 @@
+﻿using BrownianMotion.ViewModels;
+
+namespace BrownianMotion.Tests
+{
+    public class MainViewModelTests
+    {
+        [Fact]
+        public void Constructor_ShouldGenerateSimulations()
+        {
+            var vm = new MainViewModel();
+
+            Assert.Equal(vm.NumSimulations, vm.Simulations.Count);
+        }
+
+        [Fact]
+        public void Properties_ShouldHaveExpectedDefaults()
+        {
+            var vm = new MainViewModel();
+
+            Assert.Equal(20, vm.Sigma);
+            Assert.Equal(2, vm.Mean);
+            Assert.Equal(100.0, vm.InitialPrice);
+            Assert.Equal(252, vm.NumDays);
+            Assert.Equal(5, vm.NumSimulations);
+        }
+
+        [Fact]
+        public void GenerateSimulationCommand_ShouldRegenerateSimulations()
+        {
+            var vm = new MainViewModel();
+
+            var firstRun = vm.Simulations.Select(s => s.ToArray()).ToList();
+            vm.GenerateSimulationCommand.Execute(null);
+            var secondRun = vm.Simulations.Select(s => s.ToArray()).ToList();
+
+            Assert.Equal(vm.NumSimulations, secondRun.Count);
+            Assert.NotEqual(firstRun[0][1], secondRun[0][1]);
+        }
+
+        [Fact]
+        public void Simulations_ShouldContainArraysOfCorrectLength()
+        {
+            var vm = new MainViewModel();
+
+            Assert.All(vm.Simulations, sim => Assert.Equal(vm.NumDays, sim.Length));
+        }
+    }
+}
